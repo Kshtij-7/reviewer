aa27fb2b06b757c967c1dbb2819218d64e93dd74
Kshitij Lahoti <78255433+Kshtij-7@users.noreply.github.com>
2024-10-08 23:24:32 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-};
+    apiKey: "AIzaSyAPgMAC-k2lKo46OAdqKPDFdCMxkHsakok",
+    authDomain: "reviewer-3e104.firebaseapp.com",
+    projectId: "reviewer-3e104",
+    storageBucket: "reviewer-3e104.appspot.com",
+    messagingSenderId: "199147480723",
+    appId: "1:199147480723:web:887d9014a8f2ec197b0f66",
+    measurementId: "G-DGQ623776S"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
